57518c275630386d083461402e8f363254e09188
nhocconan123 <nhocconan008@gmail.com>
2026-02-01 14:40:13 +0700

    spring boot project

---
+++ src/main/java/webTruyen/WebTruye/WebTruyeApplication.java
@@ -0,0 +1,13 @@
+package webTruyen.WebTruye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTruyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTruyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webTruyen/WebTruye/WebTruyeApplicationTests.java
@@ -0,0 +1,13 @@
+package webTruyen.WebTruye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTruyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
